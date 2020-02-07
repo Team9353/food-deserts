00b42668849ace01db66c767eced713b88870251
Evan Strat <evan10s@users.noreply.github.com>
2020-02-07 11:33:39 -0500

    Commit the rest of the things

---
+++ client/src/components/map/GoogleMap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+
+function GoogleMap(props) {
+    console.log(props);
+    console.log(process.env);
+    return (
+        <div style={{height: '100vh', width: '100%'}}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+                defaultZoom={8}
+                defaultCenter={{lat: 0, lng: 0}}
+            >
+            </GoogleMapReact>
+        </div>
+    );
+}
+
+export default GoogleMap;
