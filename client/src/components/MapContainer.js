dcf3a7659c0f649acc3016b0d0b3db99e4e1bc42
skywheeler <li.hannah@gmail.com>
2020-02-17 12:32:48 -0500

    fixup! fixup! Adding the ui portion of the filters to the map page

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -2,8 +2,6 @@ import React from "react";
 import GoogleMap from "./map/GoogleMap";
 import Filters from "./Filters.js";
 import "./css/map.css";
-import Map from "./map/test.js"
-import SearchBar from "./map/SearchBar";
 
 class MapContainer extends React.Component {
     render() {
