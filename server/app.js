14d8fdc13ae095b1b35ff6d8d72fa3ec2e718c51
Evan Strat <evan10s@users.noreply.github.com>
2020-04-05 00:55:25 -0400

    Removed unnecessary files and fixed up app.js to serve frontend

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const mapRouter = require("./routes/map");
 const app = express();
 
@@ -19,11 +17,14 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/map", mapRouter);
+app.use("/api/v1/map", mapRouter);
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
